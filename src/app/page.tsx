59202280a0a5f76158d0fcbeee757728ccbba55f
oman <omanapple42@hotmail.com>
2025-12-21 17:58:51 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <Button variant="outline">Click Me</Button>
+    </main>
+  );
+}
