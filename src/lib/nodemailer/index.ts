35991ce5fea78ffcfc893e185a07777eb29a72d3
oman <omanapple42@hotmail.com>
2025-12-22 21:25:15 -0300

    feat/implemeting daily news cron

--- src/lib/nodemailer/index.ts
+++ src/lib/nodemailer/index.ts
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer"
-import { WELCOME_EMAIL_TEMPLATE } from "./templates"
+import { WELCOME_EMAIL_TEMPLATE, NEWS_SUMMARY_EMAIL_TEMPLATE } from "@/lib/nodemailer/templates";
+
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -25,3 +26,21 @@ export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData)
 
   await transporter.sendMail(mailOptions);
 }
+
+export const sendNewsSummaryEmail = async (
+    { email, date, newsContent }: { email: string; date: string; newsContent: string }
+): Promise<void> => {
+    const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
+        .replace('{{date}}', date)
+        .replace('{{newsContent}}', newsContent);
+
+    const mailOptions = {
+        from: `"Signalist News" <signalist@jsmastery.pro>`,
+        to: email,
+        subject: `📈 Market News Summary Today - ${date}`,
+        text: `Today's market news summary from Signalist`,
+        html: htmlTemplate,
+    };
+
+    await transporter.sendMail(mailOptions);
+};
